c1780c0b540a794fc9561a487d27dcb724e70daf
ZyadYhia <zeyad.yhia95@gmail.com>
2021-05-21 05:42:15 +0200

    Library laravel-project

--- resources/views/cats/show.blade.php
+++ resources/views/cats/show.blade.php
@@ -3,7 +3,7 @@
     {{$cat->name}} Category
 @endsection
 @section('main')
-    <h1>All Categories</h1>
+    <h1>{{$cat->name}} Categories</h1>
     <h2>{{$cat->id}} - {{$cat->name}}</h2>
     <p>{{$cat->desc}}</p>
     <small>Created at {{$cat->created_at}}</small>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,10 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    @yield('page-style')
 </head>
 
 <body>
     @yield('main')
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
+    @yield('page-script')
 </body>
 
 </html>
