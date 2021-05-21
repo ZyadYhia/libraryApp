8b36b45cfa6d9678a6b5bd235640b49b37d0daaf
ZyadYhia <zeyad.yhia95@gmail.com>
2021-05-21 16:07:44 +0200

    Library laravel-project

--- resources/views/errors.blade.php
+++ resources/views/errors.blade.php
@@ -1,8 +1,8 @@
 @if($errors->any())
-<div class="alert alert-danger">
+<div class="alert w-50 text-danger">
     <ul class="list-unstyled">
         @foreach($errors->all() as $error)
-        <li> {{$error}} </li>
+        <li>* {{$error}} </li>
         @endforeach
     </ul>
 </div>
